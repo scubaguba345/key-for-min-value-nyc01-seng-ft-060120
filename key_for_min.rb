f52d08f231c5825e5f5e42e6b004b4d7f78b6851
brandon gubitosa <brandon.gubitosa@gmail.com>
2020-05-19 23:58:43 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,9 +2,13 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+  if name_hash.size == 0
+    return nil
+  end
 
-  #if name_hash.size == 0
-  #  return nil
-  #end
-#end
+  name_hash.each do |key,value|
+    if name_hash.size > value
+      name_hash.size = value
+end
+name_hash
 end
