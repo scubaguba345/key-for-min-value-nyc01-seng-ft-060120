70cf208f72d64405a5b1d3bba26444e5f2e1aec7
brandon gubitosa <brandon.gubitosa@gmail.com>
2020-05-20 00:09:46 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,7 @@ def key_for_min_value(name_hash)
   if name_hash.length == 0
     return nil
   end
-  low_value = 1
+  low_value == 1
 
 
 
@@ -17,7 +17,7 @@ def key_for_min_value(name_hash)
 
   name_hash.each do |key, value|
     if value > low_value
-      value == low_value
+      value = low_value
     end
   end
 
