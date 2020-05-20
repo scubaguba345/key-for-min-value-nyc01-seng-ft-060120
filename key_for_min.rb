0579710f9c9580649c2563fd51134457ed9ce918
brandon gubitosa <brandon.gubitosa@gmail.com>
2020-05-20 00:12:38 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  if name_hash.empty?
+  if name_hash.size == 0
     return nil
   end
   low_value = 0
